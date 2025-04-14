48c5d9c2f23efb00869bc7954ae1b91fa26e8c27
Patrique Porto <patrikrock@hotmail.com>
2025-04-13 23:01:56 -0300

    Add Skills, Form & Footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,11 @@ const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 const hoverSign = document.querySelector('.hover-sign');
 
+// Sidebar Variables
+const sideBar = document.querySelector('.siderbar');
+const menu = document.querySelector('.menu-icon');
+const close = document.querySelector('.close-icon');
+
 // Project Video List
 const videoList = [video1, video2, video3];
 
@@ -17,4 +22,15 @@ videoList.forEach(video => {
         hoverSign.classList.remove('active');
     });
 });
-    
+
+// Sidebar Elements
+
+menu.addEventListener("click", function() {
+    sideBar.classList.toggle('open-sidebar');  
+});
+
+// Close Sidebar
+
+close.addEventListener("click", function() {
+    sideBar.classList.toggle('open-sidebar');
+});
